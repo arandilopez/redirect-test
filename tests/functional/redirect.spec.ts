1aea0de39130cc264552d031c8764faac0299656
Arandi Lopez <arandilopez.93@gmail.com>
2022-05-19 14:58:27 -0500

    chore: write test example

--- tests/functional/redirect.spec.ts
+++ tests/functional/redirect.spec.ts
@@ -1,5 +1,22 @@
 import { test } from '@japa/runner'
+import nock from 'nock'
 
 test.group('Redirect', () => {
-  // Write your test here
+
+  function mockPort80() {
+    nock('http://0.0.0.0')
+      .get('/')
+    .reply(200, () => {
+      return { hello: "world" }
+    })
+  }
+
+  test('redirect works as expected', async ({client}) => {
+    mockPort80()
+
+    const response = await client.get('/not-here')
+
+    console.log({response})
+    console.log(response.redirects())
+  })
 })
